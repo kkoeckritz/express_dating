9dce9a7cc38f833242a074c73779c28cf868eed0
Kris Koeckritz <kjk464@humboldt.edu>
2018-09-08 10:02:13 -0700

    add server routes

---
+++ app/routes/apiRoutes.js
@@ -0,0 +1,14 @@
+// link user data
+var friends = require("../data/friends");
+
+module.exports = function(app) {
+    // grab friend data via GET
+    app.get("/api/friends", function(req, res) {
+        res.json(friends);
+    });
+
+    // compare survey answers with others via POST
+    app.post("api/friends", function(req, res) {
+        // TODO: COMPATIBILITY LOGIC
+    });
+};
---
+++ app/routes/htmlRoutes.js
@@ -0,0 +1,12 @@
+// var data = require(data...)
+
+module.exports = function(app) {
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+
+    // default to home
+    app.get("*", function(req, res) {
+        res.sendFile("./public/home.html");
+    })
+};
